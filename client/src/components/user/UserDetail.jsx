6e8c01ad19928346e4e4a2bb12985cc21bd91817
Marianotd <marianotorresdistefano@gmail.com>
2024-09-29 20:14:43 -0300

    Cambios locales

--- client/src/components/user/UserDetail.jsx
+++ client/src/components/user/UserDetail.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function UserDetail({ user }) {
   const { first_name, last_name, email } = user
   return (
-    <div className='flex flex-col items-start justify-evenly gap-4 text-2xl border border-[#444] py-8 px-4 rounded-xl bg-[#3B82F6]'>
+    <div className='flex flex-col items-start justify-evenly gap-4 text-lg border border-[#444] py-8 px-4 rounded-xl bg-[#3B82F6]'>
       <div className='w-full p-4 bg-white rounded-xl flex flex-col lg:flex-row items-center gap-2 border border-[#444]'>
         <h3>Nombre: </h3>
         <span className='font-bold max-w-full lg:max-w-[60%] truncate'>{first_name}</span>
