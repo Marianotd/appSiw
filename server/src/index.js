ead778df33c6a9ed7dc127ce820750de2b2892b0
Marianotd <marianotorresdistefano@gmail.com>
2024-09-29 21:04:50 -0300

    Pruebas archivo vercel server

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,10 @@ import configEnv from './config/env.js'
 
 const port = configEnv.port || 4000
 
+app.get('/', (req, res) => {
+  res.send('Bienvenido.!')
+})
+
 const main = async () => {
   try {
     await app.listen(port)
